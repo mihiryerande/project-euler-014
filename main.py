631fd4a68eb394466be294e01efcf4676448e9c3
mihiryerande <mihiryerande@gmail.com>
2022-03-16 20:44:21 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,87 @@
+# Problem 13:
+#     Longest Collatz Sequence
+#
+# Description:
+#     The following iterative sequence is defined for the set of positive integers:
+#         n → n/2 (n is even)
+#         n → 3n + 1 (n is odd)
+#
+#     Using the rule above and starting with 13, we generate the following sequence:
+#         13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+#
+#     It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms.
+#
+# Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+# Which starting number, under one million, produces the longest chain?
+# NOTE: Once the chain starts the terms are allowed to go above one million.
+
+COLLATZ = dict()
+
+
+def collatz_count(x):
+    """
+    Returns the number of elements in the Collatz chain starting with `x`
+      and (presumably) ending with 1.
+
+    Args:
+        x (int): Natural number
+
+    Returns:
+        Length of Collatz chain for `x`
+
+    Raises:
+        AsserError: if incorrect params are given
+    """
+    assert type(x) == int and x > 0
+    global COLLATZ
+    if x not in COLLATZ:
+        # Not already seen, so must calc and persist
+        chain_count = 1 + (0 if x == 1 else collatz_count(x//2 if x % 2 == 0 else 3*x+1))
+        COLLATZ[x] = chain_count
+    return COLLATZ[x]
+
+
+def main(n):
+    """
+    Returns the number (less than `n`) which produces the longest Collatz chain,
+      as well as the chain itself.
+
+    Args:
+        n (int): Natural number greater than 1
+
+    Returns:
+        Tuple of number less than `n` producing the largest Collatz chain, and the chain.
+
+    Raises:
+        AssertError: if incorrect params are given
+    """
+    assert type(n) == int and n > 1
+
+    longest_count = 0
+    best_num = 0
+    for i in range(1, n):
+        this_count = collatz_count(i)
+        if this_count > longest_count:
+            best_num = i
+            longest_count = this_count
+
+    # Recompute the actual chain
+    x = best_num
+    longest_chain = [x]
+    while x > 1:
+        if x % 2 == 0:
+            x //= 2
+        else:
+            x = 3*x+1
+        longest_chain.append(x)
+
+    return best_num, longest_chain
+
+
+if __name__ == '__main__':
+    num = int(input('Enter a natural number: '))
+    start, chain = main(num)
+    print('Longest chain starting below {}:'.format(num))
+    print('  Starts at {}'.format(start))
+    print('  Length is {}'.format(len(chain)))
+    print('  Chain:\n    {}'.format('\n    → '.join(map(str, chain))))
